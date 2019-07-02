fd829ee1eaeaa3c95a5267114efaa5b8040946a8
RicardoMAlves <ricardo.machalv@gmail.com>
2019-07-01 22:29:16 -0300

    ChessConsole - Jogada Especial Promocao

--- ChessConsole/Xadrez/PartidaXadrez.cs
+++ ChessConsole/Xadrez/PartidaXadrez.cs
@@ -134,11 +134,28 @@ namespace ChessConsole.Xadrez
         public void RealizaJogada(Posicao origem, Posicao destino)
         {
             Peca PecaCapturada = ExecutaMovimento(origem, destino);
+
             if (EstaEmXeque(JogadorAtual))
             {
                 DesfazMovimento(origem, destino, PecaCapturada);
                 throw new TabuleiroException("Você não pode se colocar em xeque!");
             }
+
+            Peca p = Tab.APeca(destino);
+
+            // Jogada Especial Promoção
+            if (p is Peao)
+            {
+                if ((p.Cor == Cor.Branca && destino.Linha == 0) || (p.Cor == Cor.Preta && destino.Linha == 7))
+                {
+                    p = Tab.RetirarPeca(destino);
+                    Pecas.Remove(p);
+                    Peca dama = new Dama(Tab, p.Cor);
+                    Tab.ColocarPeca(dama, destino);
+                    Pecas.Add(dama);
+                }
+            }
+
             if (EstaEmXeque(Adversaria(JogadorAtual)))
             {
                 Xeque = true;
@@ -156,8 +173,6 @@ namespace ChessConsole.Xadrez
                 Turno++;
                 MudaJogador();
             }
-
-            Peca p = Tab.APeca(destino);
 
             // Jogada Especial En Passant
             if (p is Peao && (destino.Linha == origem.Linha - 2 || destino.Linha == origem.Linha +2))
